6e4c87ae83076b280bcef06d3b442917d541012c
Marina8989 <marina.mill209@gmail.com>
2020-01-14 20:52:12 -0800

    worked on some js

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 //variables
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.products-center');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 
 //main cart
 let cart = [];
 
-
 class Products {
     async getProducts() {
         try {
-            let result = await fetch('products.json');
+            let result = await fetch("products.json");
             let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image };
+            });
+            return products;
         } catch (error) {
             console.log(error);
         }
-
     }
 }
 // display products
-class UI {
-
-}
+class UI {}
 
 //local storage
-class Storage {
-
-}
+class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
@@ -41,4 +42,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //get all products
     products.getProducts().then(data => console.log(data));
-})
+});
